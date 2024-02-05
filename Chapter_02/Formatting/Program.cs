265309053c4c04e42fdf029e157840ac140edfb4
thsdias <thdias.84@gmail.com>
2024-02-05 17:20:39 -0300

    Chapter 2

---
+++ Chapter_02/Formatting/Program.cs
@@ -0,0 +1,86 @@
+﻿using static System.Console;
+
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+Formatting();
+InterpolatedStrings();
+AlignmentValues();
+GettingTextInput();
+GettingTextInputII();
+
+void Formatting()
+{
+    WriteLine(
+        format: "{0} apples cost {1:c}",
+        arg0: numberOfApples,
+        arg1: pricePerApple * numberOfApples
+    );
+
+    // Three parameter values can use named arguments.
+    WriteLine(
+        "{0} {1} lived in {2}.", 
+        arg0: "Roger", arg1: "Cevung", arg2: "Stockholm"
+    );
+
+    // Four or more parameter values cannot use named arguments.
+    WriteLine(
+        "{0} {1} lived in {2} and worked in the {3} team at {4}.", 
+        "Roger", "Cevung", "Stockholm", "Education", "Optimizely"
+    );
+}
+
+void InterpolatedStrings()
+{
+    // The following statement must be all on one line when using C# 10
+    // or earlier. If using C# 11 or later, we can include a line break
+    // in the middle of an expression but not in the string text.
+    WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+}
+
+void AlignmentValues()
+{
+    // An N0 format string means a number with thousand separators and no decimal places, 
+    // while a C format string means currency. The currency format will be determined by the current thread.
+
+    string applesText = "Apples"; 
+    int applesCount = 1234;
+    string bananasText = "Bananas"; 
+    int bananasCount = 56789;
+
+    WriteLine();
+    WriteLine(
+        format: "{0,-10} {1,6}",
+        arg0: "Name", arg1: "Count");
+    
+    WriteLine(
+        format: "{0,-10} {1,6:N0}",
+        arg0: applesText, arg1: applesCount);
+    
+    WriteLine(
+        format: "{0,-10} {1,6:N0}",
+        arg0: bananasText, arg1: bananasCount);
+
+    WriteLine(
+        format: "{0,-10} {1,6:C0}",
+        arg0: bananasText, arg1: bananasCount);
+} 
+
+void GettingTextInput()
+{
+    Write("Type your first name and press ENTER: "); 
+    string? firstName = ReadLine();
+    Write("Type your age and press ENTER: "); 
+    string age = ReadLine()!;
+    WriteLine($"Hello {firstName}, you look good for {age}.");
+}
+
+void GettingTextInputII()
+{
+    Write("Press any key combination: "); 
+    ConsoleKeyInfo key = ReadKey(); 
+    WriteLine();
+    WriteLine(
+        "Key: {0}, Char: {1}, Modifiers: {2}",
+        arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # C# 12 and .NET 8
 Modern Cross-Platform Development Fundamentals: Start building websites and services with ASP.NET Core 8, Blazor, and EF Core 8 8th ed. Edition
+
+- ##### Chapter 1: Hello, C#! Welcome, .NET!
+> 
+    - Set up your development environment.
+    - Discussed the similarities and differences between modern .NET, .NET Core, .NET Framework, Xamarin, and .NET Standard in an online article.
+    - Used Visual Studio 2022 and Visual Studio Code with the .NET SDK CLI to create a couple of simple console apps grouped in a solution.
+    - Learned how to download the solution code for this book from its GitHub repository.
+    - Learned how to find help. This could be in the traditional way, by using help command switches, documentation, and articles, or the modern way, by having a conversation with a coding expert AI, or using an AI-based tool to perform “grunt work.”
+
+- ##### Chapter 2: Speaking C#
+> 
+    - Declare variables with a specified or inferred type.
+    - Use some of the built-in types for numbers, text, and Booleans.
+    - Choose between number types.
+    - Control output formatting in console apps.
